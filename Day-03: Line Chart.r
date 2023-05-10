585b4cab18cfadcc71ddadefbf5700ccf15dbf15
Manohar Javvaji <112223392+Balu2377@users.noreply.github.com>
2023-05-10 09:21:29 +0530

    Create Day-03: Line Chart.r

---
+++ Day-03: Line Chart.r
@@ -0,0 +1,8 @@
+# Create a vector of x-axis values
+x <- 1:10
+
+# Create a vector of y-axis values
+y <- c(2, 4, 6, 8, 10, 12, 14, 16, 18, 20)
+
+# Create a line chart with the x and y values
+plot(x, y, type = "l", xlab = "X Values", ylab = "Y Values", main = "Line Chart Example")
